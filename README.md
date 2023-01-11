20916bd4c78034917125837cdcf41b710af750c7
Poxidq <yarullin_vadim@mail.ru>
2023-01-11 20:31:22 +0500

    Created base structure of the project / controllers for the posts / connected mysql database ☯

--- README.md
+++ README.md
@@ -1,11 +1,41 @@
-# This is just a sample blog project 
+# This is just a sample blog project
 
 ### Spices:
-* React.js
-* styled-components
-* Node.js
-* Express.js
-* TypeScript
-* Vite.js
 
-...
+- React.js
+- styled-components
+- Node.js
+- Express.js
+- TypeScript
+- Vite.js
+- ...
+
+### How to create tables in MySQL
+
+```sql
+-- table for user
+CREATE TABLE `blog_app`.`users`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username`VARCHAR(45) NOT NULL
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `img` varchar(255) NULL,
+    PRIMARY KEY (`id`),
+);
+-- table for post (how unexpected)
+CREATE TABLE `blog_app`.`posts` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `desc` VARCHAR(1000) NOT NULL,
+  `img` VARCHAR(255) NOT NULL,
+  `date` DATETIME NOT NULL,
+  `uid` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `uid_idx` (`uid` ASC) VISIBLE,
+  CONSTRAINT `uid`
+    FOREIGN KEY (`uid`)
+    REFERENCES `blog_app`.`users` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
+
+```
--- api/src/app.ts
+++ api/src/app.ts
@@ -2,15 +2,22 @@ import cors from "cors";
 import express from "express";
 import helmet from "helmet"; // Helmet helps you secure your Express apps by setting various HTTP headers.
 import morgan from "morgan"; // HTTP request logger middleware for node.js
+import cookieParser from "cookie-parser";
+import multer from "multer"; // uploading files
+
 import config from "./config";
 import errorHandler from "./middleware/errorHandler";
 import fourOhFour from "./middleware/fourOhFour";
-import root from "./routes/root";
+
+import postRoutes from "./routes/posts";
+// import authRoutes from "./routes/auth";
+// import userRoutes from "./routes/users";
 
 const app = express();
 
 // Apply most middleware first
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
@@ -21,8 +28,29 @@ app.use(
 app.use(helmet());
 app.use(morgan("tiny"));
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  if (file != undefined) {
+    res.status(200).json(file.filename);
+  } else {
+    res.status(200).json({ message: "Upload failed" });
+  }
+});
 // Apply routes before error handling
-app.use("/", root);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 // Apply error handling last
 app.use(fourOhFour);
